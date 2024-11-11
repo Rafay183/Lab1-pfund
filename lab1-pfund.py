76714971e40f73450b190ce41996a44b279391c6
Muhammad Rafay Shaikh <50517621+Rafay183@users.noreply.github.com>
2024-11-11 15:47:48 +0500

    lab 1-pfund

---
+++ lab1-pfund.py
@@ -0,0 +1,36 @@
+''''print("::::::::::::pet suggestions:::::::::::")
+
+size=input("Enter a size of pet you want? (small/large)").lower()
+activity=input("Enter a type of pet you want? (aggressive/calm)").lower()
+if size=="small" and activity=="calm":
+    pet="A calm kitten 🐱"
+elif size=="small" and activity=="aggressive":
+    pet="A wild Cat"
+elif size=="large" and activity=="calm":
+    pet="A calm Goat"
+elif size=="large" and activity=="aggressive":
+    pet="german shephard"   
+else:
+    pet=" A low maintanance fish"
+    
+print(pet)'''
+
+
+#program 2
+
+
+'''
+print("hello")'''
+
+
+ #proogram 3
+
+
+
+
+
+
+
+
+
+    
